dd64e0b6615f8ac260f1815a2f405de2070a91b0
Gardego5 <garrett.davis.7777@gmail.com>
2022-04-24 18:26:14 -0700

    Initial Commit

---
+++ modules/Calculator.js
@@ -0,0 +1,283 @@
+export default class Calculator {
+    constructor(mainField, stackFields) {
+        this.stack = [];
+        this.currentValue = 0;
+        this.displayedValue = "";
+
+        this.shouldPushMainField = false;
+        this.currencyMode = false;
+
+        this.mainField = document.getElementById("stack-0");
+        this.stackFields = [
+            document.getElementById("stack-1"),
+            document.getElementById("stack-2"),
+            document.getElementById("stack-3"),
+        ];
+        this.counterField = document.getElementById("stack-counter");
+    }
+
+    addDigit(digit) {
+        if (this.shouldPushMainField) {
+            this.pushNumberToStack(false);
+            this.displayedValue = "";
+            this.shouldPushMainField = false;
+        } else if (this.shouldClearMainField) {
+            this.displayedValue = ""
+            this.currentValue = 0;
+            this.shouldClearMainField = false;
+        }
+
+        let afterAddingDigit = this.displayedValue + String(digit);
+        
+        if (validNumber(afterAddingDigit)) {
+            this.displayedValue = afterAddingDigit;
+            this.currentValue = Number(afterAddingDigit);
+        }
+
+        this.render();
+    }
+
+    setNumber(number) {
+        if (this.shouldPushMainField) {
+            this.pushNumberToStack(false);
+            this.displayedValue = "";
+            this.shouldPushMainField = false;
+        } else if (this.shouldClearMainField) {
+            this.displayedValue = ""
+            this.currentValue = 0;
+            this.shouldClearMainField = false;
+        }
+
+        this.currentValue = number;
+        this.displayedValue = this.currentValue;
+
+        this.render();
+    }
+
+    clear() {
+        this.currentValue = 0;
+        this.displayedValue = "";
+        this.shouldPushMainField = false;
+
+        this.render();
+    }
+
+    clearAll() {
+        this.currentValue = 0;
+        this.displayedValue = "";
+        this.stack = [];
+        this.shouldPushMainField = false;
+
+        this.render();
+    }
+
+    drop() {
+        if (this.stack.length) { // If there's anything on the stack, drop the stack down
+            this.currentValue = this.stack.shift()
+            this.displayedValue = this.currentValue;
+            this.shouldPushMainField = true;
+        } else {
+            this.clear();
+        }
+
+        this.render();
+    }
+
+    roll() {
+        if (this.stack.length) {
+            const oldValue = this.currentValue;
+            this.currentValue = this.stack.shift();
+            this.displayedValue = this.currentValue;
+            this.stack.push(oldValue);
+        }
+
+        this.render();
+    }
+
+    swap() {
+        if (this.stack.length) {
+            const oldValue = this.currentValue;
+            this.currentValue = this.stack.shift();
+            this.displayedValue = this.currentValue;
+            this.stack.unshift(oldValue);
+        }
+
+        this.render();
+    }
+
+    setNormalMode() {
+        this.currencyMode = false;
+
+        this.render();
+    }
+
+    setCurrencyMode() {
+        this.currencyMode = true;
+
+        this.render();
+    }
+
+    divide() {
+        if (this.stack.length) {
+            this.stack[0] = Number(this.stack[0]) / this.currentValue;
+            this.drop();
+        }
+
+        this.render();
+    }
+
+    multiply() {
+        if (this.stack.length) {
+            this.stack[0] = Number(this.stack[0]) * this.currentValue;
+            this.drop();
+        }
+
+        this.render();
+    }
+
+    subtract() {
+        if (this.stack.length) {
+            this.stack[0] = Number(this.stack[0]) - this.currentValue;
+            this.drop();
+        }
+
+        this.render();
+    }
+
+    add() {
+        if (this.stack.length) {
+            this.stack[0] = Number(this.stack[0]) + this.currentValue;
+            this.drop();
+        }
+
+        this.render();
+    }
+
+    sqrt() {
+        this.currentValue = Math.sqrt(this.currentValue);
+        this.displayedValue = this.currentValue;
+        this.shouldPushMainField = true;
+
+        this.render();
+    }
+
+    square() {
+        this.currentValue = Math.pow(this.currentValue, 2);
+        this.displayedValue = this.currentValue;
+        this.shouldPushMainField = true;
+
+        this.render();
+    }
+
+    root() {
+        if (this.stack.length) {
+            this.stack[0] = Math.pow(this.currentValue, 1 / this.stack[0]);
+            this.drop();
+        }
+
+        this.render();
+    }
+
+    power() {
+        if (this.stack.length) {
+            this.stack[0] = Math.pow(this.currentValue, this.stack[0]);
+            this.drop();
+        }
+
+        this.render();
+    }
+
+    sin() {
+        this.currentValue = Math.sin(this.currentValue);
+        this.displayedValue = this.currentValue;
+
+        this.render();
+    }
+
+    cos() {
+        this.currentValue = Math.cos(this.currentValue);
+        this.displayedValue = this.currentValue;
+
+        this.render();
+    }
+
+    tan() {
+        this.currentValue = Math.tan(this.currentValue);
+        this.displayedValue = this.currentValue;
+
+        this.render();
+    }
+
+    swapSign() {
+        if (this.displayedValue == "-") {
+            this.displayedValue = "";
+        } else if (this.displayedValue.length) {
+            this.currentValue *= -1;
+            this.displayedValue = String(this.currentValue);
+        } else  {
+            this.displayedValue = "-";
+        }
+
+        this.render();
+    }
+
+    sum() {
+        this.currentValue = this.stack.reduce((a, b) => a + b);
+        this.displayedValue = String(this.currentValue);
+
+        this.render();
+    }
+
+    pushNumberToStack(clear = true) {
+        if (this.displayedValue) {
+            this.stack.unshift(this.currentValue);
+        }
+
+        this.shouldClearMainField = clear;
+
+        this.render();
+    }
+
+    render() {
+        // Clear all stackFields.
+        for (let field of this.stackFields) {
+            field.innerHTML = "";
+        }
+
+        // Display stack values in stackFields.
+        for (let i in this.stack.slice(0, 3)) {
+            this.stackFields[i].innerHTML = valueToDisplayedValue(this.currencyMode, this.stack[i]);
+        }
+
+        // Display the displayedValue in mainField.
+        this.mainField.innerHTML = valueToDisplayedValue(this.currencyMode, this.displayedValue);
+
+        // Show stack size in counterField.
+        this.counterField.innerHTML = this.stack.length;
+
+        // Only show Clear All button if appropriate.
+        if (this.displayedValue === "" && this.stack.length) {
+            document.getElementById("op-clear").style.display = "none";
+            document.getElementById("op-clear-all").style.display = "flex";
+        } else {
+            document.getElementById("op-clear").style.display = "";
+            document.getElementById("op-clear-all").style.display = "";
+        }
+
+        // Switch mode buttons.
+        if (this.currencyMode) {
+            document.getElementById("op-currency-mode").style.display = "none";
+            document.getElementById("op-normal-mode").style.display = "flex";
+        } else {
+            document.getElementById("op-currency-mode").style.display = "";
+            document.getElementById("op-normal-mode").style.display = "";
+        }
+
+        // Hide confirm leave page button after any interaction.
+        document.getElementById("op-home-page").style.display = "";
+        document.getElementById("op-confirm-home-page").style.display = "";
+    }
+}
+
+const validNumber = (numberString) => /^[-+]?([0-9]*[.])?[0-9]*$/.test(numberString);
+const valueToDisplayedValue = (mode, number) => (mode && validNumber(number)) ? Number(number).toFixed(2) : number;
---
+++ script.js
@@ -0,0 +1,104 @@
+import Calculator from "./modules/Calculator.js";
+
+let calculator = new Calculator();
+
+// Button EventListener
+document.getElementById("op-equals").addEventListener("click", () => calculator.pushNumberToStack());
+document.getElementById("op-clear").addEventListener("click", () => calculator.clear());
+document.getElementById("op-clear-all").addEventListener("click", () => calculator.clearAll());
+document.getElementById("op-drop").addEventListener("click", () => calculator.drop());
+document.getElementById("op-roll").addEventListener("click", () => calculator.roll());
+document.getElementById("op-swap").addEventListener("click", () => calculator.swap());
+document.getElementById("op-currency-mode").addEventListener("click", () => calculator.setCurrencyMode());
+document.getElementById("op-normal-mode").addEventListener("click", () => calculator.setNormalMode());
+
+document.getElementById("op-divide").addEventListener("click", () => calculator.divide());
+document.getElementById("op-multiply").addEventListener("click", () => calculator.multiply());
+document.getElementById("op-subtract").addEventListener("click", () => calculator.subtract());
+document.getElementById("op-add").addEventListener("click", () => calculator.add());
+
+document.getElementById("op-sign").addEventListener("click", () => calculator.swapSign());
+
+document.getElementById("op-sum").addEventListener("click", () => calculator.sum());
+
+document.getElementById("op-sqrt").addEventListener("click", () => calculator.sqrt());
+document.getElementById("op-square").addEventListener("click", () => calculator.square());
+document.getElementById("op-root").addEventListener("click", () => calculator.root());
+document.getElementById("op-power").addEventListener("click", () => calculator.power());
+
+document.getElementById("op-sin").addEventListener("click", () => calculator.sin());
+document.getElementById("op-cos").addEventListener("click", () => calculator.cos());
+document.getElementById("op-tan").addEventListener("click", () => calculator.tan());
+
+for (let digitElement of document.getElementsByClassName("digit")) {
+    if (digitElement.childNodes[0].innerHTML.charCodeAt(0) === 8455) {
+        digitElement.addEventListener("click", () => {
+            calculator.setNumber(Math.E);
+        });
+    } else if (digitElement.childNodes[0].innerHTML.charCodeAt(0) === 960) {
+        digitElement.addEventListener("click", () => {
+            calculator.setNumber(Math.PI);
+        });
+    } else {
+        digitElement.addEventListener("click", () => {
+            calculator.addDigit(digitElement.childNodes[0].innerHTML);
+        });
+    }
+}
+
+document.getElementById("op-home-page").addEventListener("click", () => {
+    document.getElementById("op-home-page").style.display = "none";
+    document.getElementById("op-confirm-home-page").style.display = "flex";
+});
+document.getElementById("op-confirm-home-page").addEventListener("click", () => window.location.href = "https://www.gjdcode.com");
+
+// Keyboard EventListener
+document.addEventListener("keypress", (event) => {
+    console.log(event.key, event.code);
+
+    switch (event.key) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case ".":
+            calculator.addDigit(event.key);
+            break;
+        case "Enter":
+        case "=":
+            calculator.pushNumberToStack();
+            break;
+        case "/":
+            calculator.divide();
+            break;
+        case "*":
+            calculator.multiply();
+            break;
+        case "-":
+            calculator.subtract();
+            break;
+        case "+":
+            calculator.add();
+            break;
+        case "d":
+        case "D":
+            calculator.drop();
+            break;
+        case "c":
+            if (calculator.mainField.innerHTML === "") {
+                calculator.clearAll();
+            } else {
+                calculator.clear();
+            }
+            break;
+        case "C":
+            calculator.clearAll();
+            break;
+    }
+});
